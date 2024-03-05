1781f79cd8f5e2352e9a59efd31699610caba870
REX <160655963+RexVpnCh@users.noreply.github.com>
2024-03-05 23:25:26 +0330

    Update script.js

--- script.js
+++ script.js
@@ -1,34 +1,22 @@
-document.addEventListener("DOMContentLoaded", function () {
-  document.querySelector(".Download-button").addEventListener("click", loadVideo);
+window.addEventListener('DOMContentLoaded', function() {
+  var videoPlayer = new Plyr('#video-player');
+  var loadVideoBtn = document.getElementById('load-video-btn');
+  var videoUrlInput = document.getElementById('video-url');
+
+  function streamVideo(url) {
+    videoPlayer.source = {
+      type: 'video',
+      sources: [
+        {
+          src: url,
+          type: 'video/mp4',
+        },
+      ],
+    };
+  }
+
+  loadVideoBtn.addEventListener('click', function() {
+    var videoUrl = videoUrlInput.value;
+    streamVideo(videoUrl);
+  });
 });
-
-function loadVideo() {
-  var url = document.getElementById("video-url").value;
-  var player = new Plyr("#player");
-
-  var videoElement = document.createElement("video");
-
-  videoElement.src = url;
-
-  videoElement.onloadedmetadata = function () {
-    if (videoElement.duration === Infinity || isNaN(videoElement.duration)) {
-      document.getElementById("error").style.display = "block";
-      document.querySelector(".video-player").style.display = "none";
-    } else {
-      player.source = {
-        type: "video",
-        sources: [
-          {
-            src: url,
-            type: "video/mp4",
-          },
-        ],
-      };
-
-      player.on("ready", function () {
-        document.getElementById("error").style.display = "none";
-        document.querySelector(".video-player").style.display = "block";
-      });
-    }
-  };
-}
